b8f94e7d5a85b204b6a75790e2a5dc06f1cf080a
fredericrous <frederic.rousseau.fr@gmail.com>
2021-07-08 22:59:21 +0100

    🐛  fix: change way to get default branch

--- hooks/pre-push-branch-pattern.zsh
+++ hooks/pre-push-branch-pattern.zsh
@@ -8,7 +8,7 @@ BRANCH_REGEX='^(feat|fix|hotfix|chore|test|automation)/\d+-[\w-]+$'
 
 LOCAL_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-if  ! git show-branch remotes/origin/$LOCAL_BRANCH &> /dev/null && ! echo $LOCAL_BRANCH | rg $BRANCH_REGEX &> /dev/null; then
+if  ! git show-branch remotes/origin/$LOCAL_BRANCH &> /dev/null && ! echo $LOCAL_BRANCH | rg $BRANCH_REGEX -oc &> /dev/null; then
     printf "$ERROR_SIGN Branch names in this project must adhere to this contract:\n    \u001b[38;5;208m${BRANCH_REGEX}\u001b[0m.
     Rename your branch with: \u001b[38;5;208mgit branch -m\u001b[0m <branch name>\n"
     exit 1
--- hooks/pre-push-pull-rebase.zsh
+++ hooks/pre-push-pull-rebase.zsh
@@ -6,11 +6,22 @@ ERROR_SIGN="  \u001b[38;5;160m\u2717\u001b[0m"
 WARNING_SIGN="  \u001b[38;5;208m!\u001b[0m"
 VALID_SIGN="  \u001b[38;5;112m\u2713\u001b[0m"
 
-DEFAULT_BRANCH=`git rev-parse --abbrev-ref origin/HEAD | sed 's@origin/@@'`
-# default branch = HEAD when there is no head yet on the server
-[[ $DEFAULT_BRANCH = "HEAD" ]] && exit 0
+BRANCH_LIST=`git branch`
+if echo $BRANCH_LIST | rg '^[\s*]+main$' --context=0 -or '$1'; then
+    DEFAULT_BRANCH="main"
+elif echo $BRANCH_LIST | rg '^[\s*]+master$' --context=0 -or '$1'; then
+    DEFAULT_BRANCH="master"
+else
+    DEFAULT_BRANCH=`git ls-remote --heads -q | rg 'refs/heads/(.*)' --context=0 -or '$1'`;
+    [[ $? -ne 0 ]] && exit 0
+fi
 
-if ! git pull --rebase origin HEAD; then
+HAS_DIVERGED=`git status -sb | rg 'ahead\s\d+,\sbehind' -c`
+if [[ $HAS_DIVERGED -eq 1 ]]; then
+    printf "$WARNING_SIGN branches diverged. Skip pull rebase\n"
+    SUGGESTED_COMMAND="git reset --soft HEAD~1 && git pull"
+    printf "    You might want to execute: \u001b[38;5;208m$SUGGESTED_COMMAND\u001b[0m\n"
+elif ! git pull --rebase origin HEAD; then
     printf "$ERROR_SIGN Couldn't complete pull rebase. You might want to do a git merge instead"
     printf "    \u001b[38;5;208mgit fetch\u001b[0m && \u001b[38;5;208mgit merge origin $DEFAULT_BRANCH\u001b[0m"
     exit 1
@@ -19,5 +30,9 @@ fi
 git fetch origin $DEFAULT_BRANCH
 AHEAD_COMMITS=`git rev-list --left-right --count origin/$DEFAULT_BRANCH...HEAD | head -c 1`
 if [[ ! $AHEAD_COMMITS = 0 ]]; then
-    printf "$WARNING_SIGN $DEFAULT_BRANCH is ahead of current branch by $AHEAD_COMMITS commits.\n    You might want to execute: git merge origin/$DEFAULT_BRANCH && git push origin HEAD\n"
+    printf "$WARNING_SIGN origin/$DEFAULT_BRANCH is ahead of current branch by $AHEAD_COMMITS commits.\n"
+    if [[ $HAS_DIVERGED -ne 1 ]]; then
+        SUGGESTED_COMMAND="git merge origin/$DEFAULT_BRANCH && git push origin HEAD"
+        printf "    You might want to execute: \u001b[38;5;208m$SUGGESTED_COMMAND\u001b[0m\n"
+    fi
 fi
