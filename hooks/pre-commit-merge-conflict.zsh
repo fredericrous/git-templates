5e8dc6da42826f1cdbea4ab65efb81b2fa4d8744
fredericrous <frederic.rousseau.fr@gmail.com>
2021-07-09 16:53:15 +0100

    🐛  fix: exlude pre-commit-merge-conflict itself

--- hooks/pre-commit-merge-conflict.zsh
+++ hooks/pre-commit-merge-conflict.zsh
@@ -3,9 +3,10 @@
 # Author: https://github.com/fredericrous
 ERROR_SIGN="  \u001b[38;5;160m\u2717\u001b[0m"
 VALID_SIGN="  \u001b[38;5;112m\u2713\u001b[0m"
+HOOK_PATH=hooks/`basename "$0"`
 
-FILES_IN_CONFLICT=(`git grep --cached  -e '>>>>>' --or -e '=======' --or -e '>>>>>>>' --all-match --files-with-matches`)
-
+FILES_IN_CONFLICT=(`git grep --cached  -e '<<<<<<<' --or -e '=======' --or -e '>>>>>>>' --all-match --files-with-matches`)
+FILES_IN_CONFLICT=${FILES_IN_CONFLICT[@]/$HOOK_PATH}
 if [[ ${#FILES_IN_CONFLICT[*]} -ne 0 ]]; then
     printf "$ERROR_SIGN Merge conflict detected in \u001b[38;5;208m${FILES_IN_CONFLICT[*]}\u001b[0m\n"
     exit 1
