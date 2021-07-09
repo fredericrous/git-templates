3144e727122f9019fafbb391584f3cf425a426bb
fredericrous <frederic.rousseau.fr@gmail.com>
2021-07-09 15:49:09 +0100

    ♻️  refactor: tell more about authorized branch name

--- hooks/pre-push-branch-pattern.zsh
+++ hooks/pre-push-branch-pattern.zsh
@@ -8,11 +8,14 @@ VALID_SIGN="  \u001b[38;5;112m\u2713\u001b[0m"
 BRANCH_REGEX='^(feat|fix|hotfix|chore|test|automation)/\d+-[\w-]+$'
 
 LOCAL_BRANCH=`git rev-parse --abbrev-ref HEAD`
-if  ! git show-branch remotes/origin/$LOCAL_BRANCH &> /dev/null; then
-    if ! echo $LOCAL_BRANCH | rg $BRANCH_REGEX -oc &> /dev/null; then
-        printf "$ERROR_SIGN Branch names in this project must adhere to this contract:\n    \u001b[38;5;208m${BRANCH_REGEX}\u001b[0m.
-        Rename your branch with: \u001b[38;5;208mgit branch -m\u001b[0m <branch name>\n"
-        exit 1
-    fi
-    printf "$VALID_SIGN Branch name conforms with authorized pattern"
+if git show-branch remotes/origin/$LOCAL_BRANCH &> /dev/null; then
+    printf "$VALID_SIGN Branch already on server. Name is authorized."
+    exit 0
 fi
+if ! echo $LOCAL_BRANCH | rg $BRANCH_REGEX -oc &> /dev/null; then
+    printf "$ERROR_SIGN Branch names in this project must adhere to this contract:\n    \u001b[38;5;208m${BRANCH_REGEX}\u001b[0m.
+        Rename your branch with: \u001b[38;5;208mgit branch -m\u001b[0m <branch name>\n"
+    exit 1
+fi
+printf "$VALID_SIGN Branch name conforms with authorized pattern"
+
