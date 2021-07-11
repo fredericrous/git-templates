67fc6ff1876942d68a1d3039881752aaa238a375
fredericrous <frederic.rousseau.fr@gmail.com>
2021-07-12 00:30:21 +0100

    🐛  fix: bug when summary not on first line

--- tests/commit-msg.test.zsh
+++ tests/commit-msg.test.zsh
@@ -22,6 +22,11 @@ COMMIT_MSG="feat: pass"
 STD_RESULT=$($HOOK_CHECK <(echo $COMMIT_MSG) 2>&1)
 echo $STD_RESULT | grep "A prefix is defined" &> /dev/null || exit 1
 
+printf "Should pass when prefix with emoji\n"
+COMMIT_MSG="👷  build: this is some summary"
+STD_RESULT=$($HOOK_CHECK <(echo $COMMIT_MSG) 2>&1)
+echo $STD_RESULT | grep "A prefix is defined" &> /dev/null || exit 1
+
 printf "Should pass when prefix with scope\n"
 COMMIT_MSG="feat(frontend): pass"
 STD_RESULT=$($HOOK_CHECK <(echo $COMMIT_MSG) 2>&1)
