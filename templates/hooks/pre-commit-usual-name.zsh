874e331feeaadc6a06149085ea26c9b42ff05a18
Frederic R <frederic.rousseau.fr@gmail.com>
2021-07-15 08:18:31 +0100

    formal english. it is instead of it's
    just for testing purposes with another tool, chezmoi

--- templates/hooks/pre-commit-usual-name.zsh
+++ templates/hooks/pre-commit-usual-name.zsh
@@ -11,5 +11,5 @@ git log -1 > /dev/null || exit 0
 
 COMMITS_PER_AUTHOR=`git shortlog -s -n -e --all`
 if ! echo $COMMITS_PER_AUTHOR | rg --context=0 "$FULL_NAME" &> /dev/null; then
-    printf "$WARNING_SIGN It's the first time you commit as \u001b[38;5;208m$FULL_NAME\u001b[0m\n"
+    printf "$WARNING_SIGN It is the first time you commit as \u001b[38;5;208m$FULL_NAME\u001b[0m\n"
 fi
